0b24aeed21686d13c99848897306b8cb65cf7c79
Bima Surya Nurwahid <bimasurya729@gmail.com>
2023-07-14 23:48:05 +0700

    create and integration auth & HomeScreen

--- app/src/main/java/com/example/tutorial/HomeAdminActivity.java
+++ app/src/main/java/com/example/tutorial/HomeAdminActivity.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeAdminActivity extends AppCompatActivity {
 
     private Button buttonLogout;
-    private TextView textViewUserId;
-
-
-
+//    private TextView textViewUserId;
     private SharedPreferences sharedPreferences;
     private static final String userId = "userId";
     private static final String P = "MyPrefs";
@@ -24,11 +20,9 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(R.layout.activity_home_admin);
         sharedPreferences = getSharedPreferences(P, MODE_PRIVATE);
 
-//        textViewUserId = findViewById(R.id.txtfullname);
         buttonLogout = findViewById(R.id.button_logout);
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,15 +32,13 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         int id = sharedPreferences.getInt(userId, 0);
-//        textViewUserId.setText("User ID: " + id);
     }
-
     private void logout() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(userId);
         editor.apply();
 
-        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        Intent intent = new Intent(HomeAdminActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
